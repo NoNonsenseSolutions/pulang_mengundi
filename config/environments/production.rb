d580187635f08f3a0377ee6617ccf01679fb54b6
You Jing Wong <wongyoujing118@gmail.com>
2018-04-11 00:13:11 +0800

    use secrets instead of credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: Rails.production[:AWS_EMAIL_DOMAIN] }
+  config.action_mailer.default_url_options = { host: ENV['AWS_EMAIL_DOMAIN'] }
   config.action_mailer.smtp_settings = {
     address: ENV['AWS_SES_SERVER_NAME'],
     port: 587,

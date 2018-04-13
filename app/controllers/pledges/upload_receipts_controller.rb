b319ebc4acaee146074b7dd899b8065192d4a044
You Jing Wong <wongyoujing118@gmail.com>
2018-04-13 11:58:04 +0800

    handle no receipt selected

--- app/controllers/pledges/upload_receipts_controller.rb
+++ app/controllers/pledges/upload_receipts_controller.rb
@@ -3,13 +3,19 @@ class Pledges::UploadReceiptsController < ApplicationController
     @pledge = Pledge.find(params[:pledge_id])
     authorize @pledge, :donor_status_update?
 
-    @pledge.receipt.attach(receipt_params[:receipt])
-    @pledge.donor_transferred!
-    @pledge.confirmed_at = Time.zone.now
-    @pledge.save
+    if params.dig(:pledge, :receipt)
+      @pledge.receipt.attach(receipt_params[:receipt])
+      @pledge.donor_transferred!
+      @pledge.confirmed_at = Time.zone.now
+      @pledge.save
 
-    flash[:success] = 'Thank you!'
-    redirect_to for_pledging_thank_you_screens_path
+      flash[:success] = 'Thank you!'
+      redirect_to for_pledging_thank_you_screens_path
+    else
+      flash[:danger] = 'Please upload a receipt to show that you have transferred'
+      redirect_back(fallback_location: @pledge)
+    end
+    
   end
 
   private

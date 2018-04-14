96aff9a2a5f8220bb9cb56b46e618f62f69ebce2
You Jing Wong <wongyoujing118@gmail.com>
2018-04-14 11:49:27 +0800

    Add social sharing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+Rails.application.routes.default_url_options[:host] = ENV['HOST_URL']
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 

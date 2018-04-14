3320c1f700e4d8831312ac8f8208079fb43d6c96
You Jing Wong <wongyoujing118@gmail.com>
2018-04-14 12:30:35 +0800

    integrate with turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,29 +36,72 @@ document.addEventListener("turbolinks:load", function(event) {
   }
 });
 
-document.addEventListener("turbolinks:load", function(){
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12&appId=2142897559273833&autoLogAppEvents=1';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12&appId=2142897559273833&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'pulangmengundi'));
 
-  window.twttr = (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0],
-      t = window.twttr || {};
-    if (d.getElementById(id)) return t;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js, fjs);
+(function($) {
+  var fbRoot;
 
-    t._e = [];
-    t.ready = function(f) {
-      t._e.push(f);
-    };
+  function saveFacebookRoot() {
+    if ($('#fb-root').length) {
+      fbRoot = $('#fb-root').detach();
+    }
+  };
 
-    return t;
-  }(document, "script", "twitter-wjs"));
-})
+  function restoreFacebookRoot() {
+    if (fbRoot != null) {
+      if ($('#fb-root').length) {
+        $('#fb-root').replaceWith(fbRoot);
+      } else {
+        $('body').append(fbRoot);
+      }
+    }
+
+    if (typeof FB !== "undefined" && FB !== null) { // Instance of FacebookSDK
+      FB.XFBML.parse();
+    }
+  };
+
+  document.addEventListener('turbolinks:request-start', saveFacebookRoot)
+  document.addEventListener('turbolinks:load', restoreFacebookRoot)
+}(jQuery));
+
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
+
+
+function renderTweetButtons() {
+  $('.twitter-share-button').each(function() {
+    var button;
+    button = $(this);
+    if (button.data('url') == null) {
+      button.attr('data-url', document.location.href);
+    }
+    if (button.data('text') == null) {
+      return button.attr('data-text', document.title);
+    }
+  });
+  return twttr.widgets.load();
+};
+
+document.addEventListener('turbolinks:load', renderTweetButtons)

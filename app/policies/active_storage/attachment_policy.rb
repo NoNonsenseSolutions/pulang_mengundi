2a70878198d0cfee77d6bd1b09470850f4629e62
You Jing Wong <wongyoujing118@gmail.com>
2018-04-18 01:18:53 +0800

    donor can delete receipt

--- app/policies/active_storage/attachment_policy.rb
+++ app/policies/active_storage/attachment_policy.rb
@@ -10,6 +10,8 @@ class ActiveStorage::AttachmentPolicy
     record = document.record
     if record.is_a? Request
       return record.requester == user
+    elsif record.is_a? Pledge
+      return record.donor == user
     end
   end
 end

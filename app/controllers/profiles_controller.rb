2a815d2d721c436db3e81debe869481f161fccfa
Jimmy Ngu <jimmynguyc@gmail.com>
2018-04-20 20:06:48 +0800

    - Enable current_user to be edited when current_admin exists - Show error when admin edits with invalid data

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,27 +4,26 @@ class ProfilesController < ApplicationController
   end
 
   def update
-    if current_admin
-      @user = User.find(params[:user_id])
-      @user.update(profile_params)
-    else  
-      @user = current_user
-      if @user.update(profile_params)
-        if params.dig(:user, :email).present? && (@user.email != params.dig(:user, :email))
-          @user.unconfirmed_email = params.dig(:user, :email)
-          if @user.save
-            UserMailer.with(user_id: @user.id).confirmation_email.deliver_later
-            flash[:success] = t('.check_email')
-          else
-            flash[:danger] = t('.invalid_email')
-          end
+    @user = User.find_by(id: params[:user_id]) || current_user
+    if @user.update(profile_params)
+      if params.dig(:user, :email).present? && (@user.email != params.dig(:user, :email))
+        @user.unconfirmed_email = params.dig(:user, :email)
+        if @user.save
+          UserMailer.with(user_id: @user.id).confirmation_email.deliver_later
+          flash[:success] = t('.check_email') if request.format.html?
         else
-          flash[:success] = t('.success')
-        end      
+          flash[:danger] = t('.invalid_email') if request.format.html?
+        end
       else
-        flash[:danger] = @user.errors.full_messages.join("; ")
+        flash[:success] = t('.success') if request.format.html?
       end
-      redirect_back_or(edit_profiles_path)
+    else
+      flash[:danger] = @user.errors.full_messages.join("; ") if request.format.html?
+    end
+
+    respond_to do |format|
+      format.json { render json: { message: @user.errors.full_messages.join("; ") }, status: @user.valid? ? :accepted : :bad_request }
+      format.html { redirect_back_or(edit_profiles_path) }
     end
   end
 

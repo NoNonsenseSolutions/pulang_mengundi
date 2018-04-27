2774a8643bbeb2307fa548b09840196e9133f8d0
You Jing Wong <wongyoujing118@gmail.com>
2018-04-28 00:30:59 +0800

    add email blast

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,13 @@ class UserMailer < ApplicationMailer
     @user.create_confirmation_token
     mail(to: @user.unconfirmed_email, subject: t('.subject'))
   end
+
+  def update_information_email
+    @email = params[:email]
+
+    attachments.inline['my_request.png'] = File.read('app/assets/images/mailer_attachments/my_request.png')
+    attachments.inline['manage_my_request.png'] = File.read('app/assets/images/mailer_attachments/manage_my_request.png')
+    attachments.inline['edit_request.png'] = File.read('app/assets/images/mailer_attachments/edit_request.png')
+    mail(to: @email, subject: "Update your details on pulangmengundi.com by May 1, 2018")
+  end
 end
